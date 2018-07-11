1fe04a5f8c49f8572ece61b123fcf1aa656a4f0b
Saurabh <saurabh.gupta@irissoftware.com>
2018-07-11 18:52:05 +0530

    spring batch boot application

---
+++ SpringBatch_Boot/src/main/java/com/iris/batch/processor/MrMarketEventProcessor.java
@@ -0,0 +1,26 @@
+package com.iris.batch.processor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import com.iris.batch.model.TradeBase;
+
+/**
+ * The Class FxMarketEventProcessor.
+ * 
+ * @author Satyveer
+ */
+public class MrMarketEventProcessor<T extends TradeBase> implements ItemProcessor<T, T> {
+
+	private static final Logger log = LoggerFactory.getLogger(MrMarketEventProcessor.class);
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public TradeBase process(final TradeBase trade) throws Exception {
+		log.info("processed trade with id " + trade.getTradeId());
+		trade.setDeviation(12.0);
+		return trade;
+	}
+
+}
