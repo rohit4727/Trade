249c32aab02c17f10b62e41057d5922f3d9c4ebf
Rohit Elayathu <rohit.elayathu@irissoftware.com>
2018-07-12 10:56:56 +0530

    Changing Constant File

---
+++ BatchJobService/src/main/java/com/iris/batchJobService/util/JobProgressConstants.java
@@ -0,0 +1,15 @@
+package com.iris.batchJobService.util;
+
+/*
+ * JobProgressConstants class for constants
+ * 
+ * @author Rohit Elayathu
+ */
+public interface JobProgressConstants {
+	
+	//Job Status 
+	public static final int RUNNING = 0;
+	public static final int COMPLETED = 1;
+	public static final int FAILED = 2;
+
+}
