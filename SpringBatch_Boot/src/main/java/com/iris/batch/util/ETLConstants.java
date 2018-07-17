17645fbd9bb8ae3903a87ad422c9f0bf9e2b2143
Rohit Elayathu <rohit.elayathu@irissoftware.com>
2018-07-17 10:48:17 +0530

    Constants renamed

--- SpringBatch_Boot/src/main/java/com/iris/batch/util/ETLConstants.java
+++ SpringBatch_Boot/src/main/java/com/iris/batch/util/ETLConstants.java
@@ -11,8 +11,8 @@ public interface ETLConstants {
 	int LINES_TO_SKIP = 1;
 	
 	// Job Status
-	int RUNNING = 0;
-	int COMPLETED = 1;
-	int FAILED = 2;
+	int JOB_RUNNING = 0;
+	int JOB_COMPLETED = 1;
+	int JOB_FAILED = 2;
 
 }
