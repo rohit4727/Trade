8622fa97100aa1ff16d6c5b00a2596356a8f15d3
Rohit <mail_to_rohit2006@yahoo.co.in>
2018-07-17 10:20:11 +0530

    Jo Id added to run the batch

--- JobScheduler/src/main/java/com/iris/scheduler/service/SchedulerService.java
+++ JobScheduler/src/main/java/com/iris/scheduler/service/SchedulerService.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.query.Param;
 
 import com.iris.scheduler.entity.JobScheduler;
-
+/**
+ * @author anchal.handa*/
 public interface SchedulerService {
 	
 	public boolean checkfilepath(String filepath);
-	public void runcmd(String filepath);
+	public void runcmd(String filepath,Long id);
 	public void savestatus(JobScheduler job);
 	public JobScheduler findbyjobId(Long id);
 	public List<JobScheduler> findbycurDate();
--- JobScheduler/src/main/java/com/iris/scheduler/service/SchedulerServiceImpl.java
+++ JobScheduler/src/main/java/com/iris/scheduler/service/SchedulerServiceImpl.java
@@ -21,49 +21,55 @@ import com.iris.scheduler.repository.JobSchedulerRepository;
 @Service
 public class SchedulerServiceImpl implements SchedulerService {
 
-	
 	private static final Logger logger = LoggerFactory.getLogger(SchedulerServiceImpl.class);
-	
-	
+
 	@Autowired
 	CronRepository cronRepository;
 	@Autowired
 	JobSchedulerRepository jobSchedulerRepository;
 	SchedulerService schedularService;
 
-	
-
-	@Override
+	/*
+	 * Check the file path/Job script path if exist
+	 */ @Override
 	public boolean checkfilepath(String path) {
-		
+
 		try {
 			File f = new File(path);
 			if (f.exists() && !f.isDirectory()) {
 				return true;
 			}
-		}
-		catch(Exception e) {
-		
-			logger.error("Failed To Run Job With Path="+path +" and Exception is "+ e.getMessage());
+		} catch (Exception e) {
+
+			logger.error("Failed To Run Job With Path=" + path + " and Exception is " + e.getMessage());
 		}
 		return false;
 
 	}
 
+	/*
+	 * Runs the batch file of different tasks/scripts with job id and job script
+	 * path
+	 */
 	@Override
-	public void runcmd(String batchfilepath) {
+	public void runcmd(String batchfilepath, Long id) {
 
 		try {
 
-			String cronrun = IControllerConstants.CRON_RUN + batchfilepath;
+			String cronrun = IControllerConstants.CRON_RUN + batchfilepath + " " + id;
 			Runtime.getRuntime().exec(cronrun);
 
 		} catch (Exception e) {
-			logger.error("Failed To Run Job With Batchfilepath="+batchfilepath+" and Exception is "+ e.getMessage());
+			logger.error(
+					"Failed To Run Job With Batchfilepath=" + batchfilepath + " and Exception is " + e.getMessage());
 		}
 
 	}
 
+	/*
+	 * To Save the status of Job in Database 1 means Run successfully 2 means
+	 * Failure and 0 means to be process as per scheduler time
+	 */
 	@Override
 	public void savestatus(JobScheduler job) {
 
@@ -71,12 +77,20 @@ public class SchedulerServiceImpl implements SchedulerService {
 
 	}
 
+	/*
+	 * Find the Jobs by Id
+	 * 
+	 */
 	@Override
 	public JobScheduler findbyjobId(Long id) {
 
 		return cronRepository.findbyjobId(id);
 	}
 
+	/*
+	 * Find the Jobs by CURRENT DATETIME
+	 * 
+	 */
 	@Override
 	public List<JobScheduler> findbycurDate() {
 
