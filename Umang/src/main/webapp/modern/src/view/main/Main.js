0f1906d0088c54eff67b56196fb2c3b3aeccc4d9
Umang Goel <umang121@gmail.com>
2018-07-24 08:33:31 +0530

    ui

--- Umang/src/main/webapp/modern/src/view/main/Main.js
+++ Umang/src/main/webapp/modern/src/view/main/Main.js
@@ -24,33 +24,6 @@ Ext.define('ui.view.main.Main', {
     tabBarPosition: 'bottom',
 
     items: [
-        // TODO - Replace the content of this view to suit the needs of your application.
-        {
-            title: 'Home',
-            iconCls: 'x-fa fa-home',
-            layout: 'fit',
-            // The following grid shares a store with the classic version's grid as well!
-            items: [{
-                xtype: 'mainlist'
-            }]
-        },{
-            title: 'Users',
-            iconCls: 'x-fa fa-user',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        },{
-            title: 'Groups',
-            iconCls: 'x-fa fa-users',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        },{
-            title: 'Settings',
-            iconCls: 'x-fa fa-cog',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        }
+        
     ]
 });
