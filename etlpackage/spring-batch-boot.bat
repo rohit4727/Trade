f7c9b9a5fce03b59d83dc853698f4d89d3faeb09
Saurabh <saurabh.gupta@irissoftware.com>
2018-07-31 15:54:16 +0530

    updated jar

--- etlpackage/spring-batch-boot.bat
+++ etlpackage/spring-batch-boot.bat
@@ -1,5 +1,1 @@
-cd E:\Trade\SpringBatch_Boot
-mvn clean package -DskipTests
-cd E:\etlpackage
-copy E:\Trade\SpringBatch_Boot\target\spring-batch-boot-0.1.0.jar *
-java -jar spring-batch-boot-0.1.0.jar
+cmd /c start "Spring batch boot" java -jar spring-batch-boot-0.1.0.jar
